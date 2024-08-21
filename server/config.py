6eb9fd91367ae566ca2ea792db828b2d3c0197af
vash.work.14 <vash.work.14@gmail.com>
2024-08-21 10:46:23 +0500

    add clickhouse logs

--- server/config.py
+++ server/config.py
@@ -7,3 +7,8 @@ class Config:
     CACHE_TYPE = 'redis'
     CACHE_REDIS_URL = REDIS_URL
     CACHE_DEFAULT_TIMEOUT = 600  # 10 минут
+
+    CLICKHOUSE_HOST = 'localhost'
+    CLICKHOUSE_PORT = 9000
+    CLICKHOUSE_USER = 'default'
+    CLICKHOUSE_PASSWORD = ''
--- server/controllers/book_controller.py
+++ server/controllers/book_controller.py
@@ -2,15 +2,30 @@ from flask import Blueprint, request, jsonify
 from bson import ObjectId
 from services.book_service import BookService
 from extensions import cache
+from clickhouse_driver import Client
 
 book_bp = Blueprint('book', __name__)
 book_service = BookService(cache)
+client = Client(host='localhost', port=9000, user='default', password='')
 
 @book_bp.route('', methods=['POST'])
 def create_book():
     data = request.get_json()
     try:
         user = book_service.create_book(data)
+
+        title = data.get('title')
+        year = data.get('year')
+        author = data.get('author')
+        create_user = 'test'
+
+        query = f'''
+            INSERT INTO logs.book_logs (title, year, author, create_user)
+            VALUES ('{title}', {year}, '{author}', '{create_user}')
+        '''
+        
+        client.execute(query)
+
         return jsonify(user), 201
     except Exception as e:
         print(f'Error creating book: {e}')
